8d3416e5a94d9231b0740aca63fa280de29c9fc9
jtitor <1165137798@qq.com>
2024-08-15 01:23:36 +0800

    commit

--- src/standard/main.py
+++ src/standard/main.py
@@ -27,9 +27,9 @@ from flax.serialization import msgpack_serialize
 from flax.training.common_utils import shard
 from torch.utils.data import DataLoader
 
-from dataset import create_dataloaders
+from ..dataset import create_dataloaders
 from training import TrainState, create_train_state, training_step, validation_step
-from utils import AverageMeter, save_checkpoint_in_background
+from ..utils import AverageMeter, save_checkpoint_in_background
 
 warnings.filterwarnings("ignore")
 
