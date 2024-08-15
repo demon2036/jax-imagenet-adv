0829f66618e061d54285bcb26e010f4db7f1c2e7
jtitor <1165137798@qq.com>
2024-08-15 20:52:21 +0800

    commit

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -33,7 +33,7 @@ def pgd_attack(image, label, state, params, epsilon=4 / 255, step_size=4 / 3 / 2
     image_perturbation = jax.random.uniform(key,image.shape,minval=-epsilon,maxval=epsilon)
 
     def adversarial_loss(perturbation):
-        logits, labels = state.apply_fn({"params": params}, image + perturbation)
+        logits = state.apply_fn({"params": params}, image + perturbation)
         loss_value = jnp.mean(softmax_cross_entropy_with_integer_labels(logits, label))
         return loss_value
 
