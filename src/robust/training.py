a82ba9f86e95d220fb184e0a10ab4835292095a8
jtitor <1165137798@qq.com>
2024-08-15 20:22:52 +0800

    commit

--- src/robust/training.py
+++ src/robust/training.py
@@ -90,31 +90,18 @@ class TrainModule(nn.Module):
 
         return logits, labels
 
-    # def train_func(self, images: Array, labels: Array, det: bool = True) -> ArrayTree:
+    # def pgd_step(self, images: Array, det: bool = True) -> ArrayTree:
     #     # Normalize the pixel values in TPU devices, instead of copying the normalized
     #     # float values from CPU. This may reduce both memory usage and latency.
     #     # images, labels = batch
     #     images = jnp.moveaxis(images, 1, 3).astype(jnp.float32) / 0xFF
+    #     logits = self.model(images, det=det)
     #
-    #     images = jax.lax.stop_gradient(pgd_attack(images, labels, self.model, ))
-    #
-    #     labels = nn.one_hot(labels, self.model.labels) if labels.ndim == 1 else labels
-    #     labels = labels.astype(jnp.float32)
-    #
-    #     if not det:
-    #         labels = optax.smooth_labels(labels, self.label_smoothing)
-    #         images, labels = self.mixup(images, labels)
-    #
-    #     loss = self.criterion((logits := self.model(images, det=det)), labels)
-    #     labels = labels == labels.max(-1, keepdims=True)
-    #
-    #     # Instead of directly comparing the maximum classes of predicted logits with the
-    #     # given one-hot labels, we will check if the predicted classes are within the
-    #     # label set. This approach is equivalent to traditional methods in single-label
-    #     # classification and also supports multi-label tasks.
-    #     preds = jax.lax.top_k(logits, k=5)[1]
-    #     accs = jnp.take_along_axis(labels, preds, axis=-1)
-    #     return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1)}
+    #     return logits
+
+
+
+
 
 
 @partial(jax.pmap, axis_name="batch", donate_argnums=0)
