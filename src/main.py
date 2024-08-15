9f8b62192a9bca1b7a99fbfbdaecb17a793369d2
jtitor <1165137798@qq.com>
2024-08-15 23:11:57 +0800

    commit

--- src/main.py
+++ src/main.py
@@ -82,7 +82,7 @@ def main(args: argparse.Namespace):
 
         # state, metrics = training_step(state, batch)
         # img = jax.pmap(pgd_attack)(batch[0], batch[1], state, key=key)
-        out = test(img, state,batch[1],key=key)
+        out = test(batch[0], batch[1], state, key=key)
         # pgd_attack(b)
 
     for step in tqdm.trange(1, args.training_steps + 1, dynamic_ncols=True):
