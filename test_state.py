e7e7e664e7a46a5a5fc23730d7e04b3f661da242
jtitor <1165137798@qq.com>
2024-08-22 09:01:16 +0800

    commit

--- test_state.py
+++ test_state.py
@@ -102,7 +102,7 @@ def create_train_state(train_state_config, image_size: int = 224, warmup_steps=1
 
 
 if __name__ == "__main__":
-    yaml = read_yaml('configs/test.yaml')
+    yaml = read_yaml('configs/convnext-s.yaml')
     yaml = preprocess_config(yaml)
 
     print(os.environ.get('GCS_DATASET_DIR'))
