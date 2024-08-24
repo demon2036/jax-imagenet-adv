271acabef9df517f52295a00f5197a8f853fa824
jtitor <1165137798@qq.com>
2024-08-24 12:07:58 +0800

    commit

--- main_adv.py
+++ main_adv.py
@@ -164,7 +164,7 @@ if __name__ == "__main__":
     # jax.distributed.initialize()
     # main(parser.parse_args())
 
-    yaml = read_yaml('configs/adv/convnext-t.yaml')
+    yaml = read_yaml('configs/adv/convnext-t-6step.yaml')
     yaml = preprocess_config(yaml)
 
     main(yaml)
