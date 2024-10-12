03ab95a53cab60ab82ebaf14607df22514c442ff
jtitor <1165137798@qq.com>
2024-10-12 18:49:42 +0800

    commit

--- go.sh
+++ go.sh
@@ -1,8 +1,3 @@
-export GCS_DATASET_DIR=$GCS_DATASET_DIR GCS_MODEL_DIR=$GCS_MODEL_DIR  WANDB_API_KEY=ec6aa52f09f51468ca407c0c00e136aaaa18a445;
-rm -rf  ADV-ViT;git clone -b test3 https://github.com/demon2036/jax-imagenet-adv ADV-ViT
-cd  ADV-ViT
-
-# 使用 for 循环遍历 SCRIPT_PATHS 数组
 for script in "${SCRIPT_PATHS[@]}"; do
     echo " $script"
     pkill -9 -f python
