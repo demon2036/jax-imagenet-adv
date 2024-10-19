ffc0dcca816b4066002c3c0b2c1e08adb4c70160
jtitor <1165137798@qq.com>
2024-10-19 08:19:47 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -31,11 +31,11 @@ eval "$(~/miniconda3/bin/conda shell.bash hook)"
 # 2. Install requirements.
 #pip install -U jax[tpu]==0.4.31 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-#pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-#pip install -U flax
-#pip install -U webdataset timm wandb
-#pip install einops gcsfs tensorflow matplotlib diffusers
-#pip install -U flax optax
+pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install -U flax
+pip install -U webdataset timm wandb
+pip install einops gcsfs tensorflow matplotlib diffusers
+pip install -U flax optax
 
 #
 ## 3. Install Pillow-SIMD.
