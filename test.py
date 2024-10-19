768d453c80aa4aa62509c44cb5cf6fd1864ed403
jtitor <1165137798@qq.com>
2024-10-19 08:14:22 +0800

    commit

--- test.py
+++ test.py
@@ -11,12 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
+import time
 
 import jax
 
-
+time.sleep(10)
 
 jax.distributed.initialize(initialization_timeout=10000)
 # print(jax.devices())
