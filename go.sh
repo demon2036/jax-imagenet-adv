3ae583182728b20464ccf9ec126947b99f8c8676
jtitor <1165137798@qq.com>
2024-10-19 10:03:31 +0800

    commit

--- go.sh
+++ go.sh
@@ -11,8 +11,8 @@ for script in "${SCRIPT_PATHS[@]}"; do
     pkill -9 -f python
     sudo rm /tmp/libtpu_lockfile
 #    source ~/miniconda3/bin/activate base;
-    python -u main_adv.py --yaml-path $script
-##    python -u main_test.py --yaml-path $script
+#    python -u main_adv.py --yaml-path $script
+    python -u main_test.py --yaml-path $script
 #    python -u test.py #--yaml-path $script
 
 done
