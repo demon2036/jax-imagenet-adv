60c7cdaef18c509205f732f52c6cc4fc20fed795
jtitor <1165137798@qq.com>
2024-10-19 09:09:13 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -30,19 +30,17 @@ eval "$(~/miniconda3/bin/conda shell.bash hook)"
 
 # 2. Install requirements.
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-#pip install -U jax[tpu]==0.4.31 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-#pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-#pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-##pip install -U flax
+pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install -U flax optax chex
 #
-#pip install -U webdataset timm wandb
-#pip install einops gcsfs tensorflow
+pip install -U webdataset timm wandb
+pip install einops gcsfs tensorflow
 #pip install "flax[all]"
 
-#pip install -U flax optax
+#pip install -U flax
 
 #
 ## 3. Install Pillow-SIMD.
-#conda install -c conda-forge -y libjpeg-turbo
-#pip uninstall -y pillow
-#CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
+conda install -c conda-forge -y libjpeg-turbo
+pip uninstall -y pillow
+CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
