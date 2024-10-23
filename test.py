1c8b45defea17fcaaa28f3e7a2de9f642cb80483
jtitor <1165137798@qq.com>
2024-10-23 19:46:15 +0800

    commit

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import jax
-print(jax.process_index())
-
-with open('test.txt','w') as f:
-    f.write(str(jax.process_index()))
 
 
-jax.distributed.shutdown()
 jax.distributed.initialize()
+print(1)
