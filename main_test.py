b96abeac977699b32d3fd839d6ea66e834f6f71a
jtitor <1165137798@qq.com>
2024-11-18 21:44:46 +0800

    commit

--- main_test.py
+++ main_test.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     # jax.distributed.initialize()
     # main(parser.parse_args())
     jax.distributed.initialize()
-    yaml = read_yaml('configs/convformer-b36.yaml.yaml')
+    yaml = read_yaml('configs/convformer-b36.yaml')
     yaml = preprocess_config(yaml)
 
     main(yaml)
