3deb5b4d328886c75761eb3bbf9804bded790e34
jtitor <1165137798@qq.com>
2024-12-01 12:00:46 +0800

    commit

--- test_dataset_fork2.py
+++ test_dataset_fork2.py
@@ -90,7 +90,7 @@ class DynamicMixRatioState:
         y_s=torch.stack([*syn_y,*y])
 
         if jax.process_index()==0:
-            print(x.shape,torch.stack(syn_x).shape,torch.stack(x).shape)
+            print(x_s.shape,torch.stack(syn_x).shape,torch.stack(x).shape)
 
         return x_s, y_s
 
