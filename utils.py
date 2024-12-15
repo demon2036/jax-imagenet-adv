ad9d9be17233595e66cdc18023597e020294cee3
jtitor <1165137798@qq.com>
2024-12-15 19:41:39 +0800

    commit

--- utils.py
+++ utils.py
@@ -309,12 +309,12 @@ def get_partition_rules():
         ('MetaFormerStage_[01]/.*/fc1/kernel', PS(None,None,'fsdp', 'mp')),
         ('MetaFormerStage_[01]/.*/fc2/kernel', PS(None,None,'mp', 'fsdp')),
 
-        # ('MetaFormerStage_[23]/.*/qkv/kernel', PS('fsdp', 'mp')),
-        # ('MetaFormerStage_[23]/.*/proj/kernel', PS('mp', 'fsdp')),
+        ('MetaFormerStage_[23]/.*/qkv/kernel', PS('fsdp', 'mp')),
+        ('MetaFormerStage_[23]/.*/proj/kernel', PS('mp', 'fsdp')),
 
 
-        # ('MetaFormerStage_[23]/.*/fc1/kernel',PS('fsdp','mp')),
-        # ('MetaFormerStage_[23]/.*/fc2/kernel', PS('mp', 'fsdp')),
+        ('MetaFormerStage_[23]/.*/fc1/kernel',PS('fsdp','mp')),
+        ('MetaFormerStage_[23]/.*/fc2/kernel', PS('mp', 'fsdp')),
         ('.*', PS(None)),
     )
 
