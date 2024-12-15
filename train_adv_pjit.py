1016b34509c57cc74d59575aeace7ba57d7c6b3a
jtitor <1165137798@qq.com>
2024-12-15 20:47:25 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -138,8 +138,11 @@ def main(configs):
                                                                         training_steps=training_steps,
                                                                         grad_accum_steps=grad_accum_steps,mesh=mesh)
 
-        training_step_pjit=pjit(training_step,static_argnums=(2,),
-                                donate_argnums=(0,),in_shardings=(train_state_partition,None,),out_shardings=(train_state_partition,P()))
+        train_state_sharding=jtu.tree_map(lambda x:NamedSharding(mesh,x),train_state_partition)
+
+
+        training_step_pjit=jax.jit(training_step,static_argnums=(2,),
+                                donate_argnums=(0,),in_shardings=(train_state_sharding,None,),out_shardings=(train_state_sharding,P()))
 
 
 
