9a438c5ac9379d44de9785300712ffe7e07676b7
jtitor <1165137798@qq.com>
2024-12-15 18:16:17 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -100,7 +100,7 @@ def main(configs):
                                                                         grad_accum_steps=grad_accum_steps,mesh=mesh)
 
         training_step_pjit=pjit(training_step,static_argnums=(2,),
-                                donate_argnums=(0,),in_shardings=(train_state_partition,P('dp'),P()),out_shardings=(train_state_partition,P()))
+                                donate_argnums=(0,),in_shardings=(train_state_partition,P('dp'),),out_shardings=(train_state_partition,P()))
 
         sharding = jax.sharding.NamedSharding(
             mesh, jax.sharding.PartitionSpec("dp"))
