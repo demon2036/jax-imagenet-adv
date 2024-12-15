5dc1f33b023de2e5946933a5c1b3b2e99df3447b
jtitor <1165137798@qq.com>
2024-12-15 21:19:46 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -180,7 +180,9 @@ def main(configs):
                 batch = jtu.tree_map(go_jit, batch)
                 images, labels = batch
                 # jnp.array().addressable_shards(0)
-                print(images.addressable_data(0).shape,images.addressable_shards)
+                print(images.addressable_data(0).shape,
+                      # images.addressable_shards
+                      )
 
                 while True:
                     pass
