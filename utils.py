69597ac69ddac0ed1130c50e5818aa59e3af2412
john <1165137798@qq.com>
2024-12-16 15:24:11 +0800

    commit

--- utils.py
+++ utils.py
@@ -299,9 +299,17 @@ def preprocess_config(yaml):
     return yaml
 
 
-
 def get_partition_rules():
     return (
+
+        ('scale', PS('mp')),
+        ('bias', PS('mp')),
+
+        ('fc/fc1/kernel', PS('fsdp', 'mp')),
+        ('fc/fc2/kernel', PS('mp', 'fsdp')),
+        ('downsample/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+        ('stem/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+
         ('MetaFormerStage_[01]/.*/pwconv1/kernel', PS(None, None, 'fsdp', 'mp')),
         ('MetaFormerStage_[01]/.*/dwconv/kernel', PS(None, None, 'fsdp', 'mp')),
         ('MetaFormerStage_[01]/.*/pwconv2/kernel', PS(None, None, 'fsdp', 'mp')),
