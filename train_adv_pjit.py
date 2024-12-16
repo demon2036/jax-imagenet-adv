dd2be06321626f83463b74a6ed7edaf1de5e6ef8
john <1165137798@qq.com>
2024-12-16 18:57:46 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -108,7 +108,7 @@ def main(configs):
         # os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=8'
         # jax.config.update('jax_platform_name', 'cpu')
         pass
-        # jax.distributed.initialize()
+        jax.distributed.initialize()
 
     use_pgd = configs.pop('use_pgd', True)
     grad_accum_steps = configs.pop('grad_accum_steps', 1)
