cbc604a732f5a048045820286781a33ffc3f104b
john <1165137798@qq.com>
2024-12-16 18:35:26 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -101,10 +101,10 @@ def main(configs):
 
 
     if use_orbax_save:
-        os.environ['JAX_PLATFORMS']='cpu'
+        # os.environ['JAX_PLATFORMS']='cpu'
 
-        os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=8'
-        jax.config.update('jax_platform_name', 'cpu')
+        # os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=8'
+        # jax.config.update('jax_platform_name', 'cpu')
         pass
         # jax.distributed.initialize()
 
