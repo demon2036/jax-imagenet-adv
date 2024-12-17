8205e55a60399e64043ccf740618ffef60d533da
john <1165137798@qq.com>
2024-12-17 20:08:18 +0800

    commit

--- utils.py
+++ utils.py
@@ -300,27 +300,27 @@ def preprocess_config(yaml):
 
 def get_partition_rules():
     return (
-        # ('scale', PS('mp')),
-        # ('bias', PS('mp')),
-        #
-        # ('fc/fc1/kernel', PS('fsdp', 'mp')),
-        # ('fc/fc2/kernel', PS('mp', 'fsdp')),
-        # ('downsample/conv/kernel', PS(None, None, 'fsdp', 'mp')),
-        # ('stem/conv/kernel', PS(None, None, 'fsdp', 'mp')),
-        #
-        # ('MetaFormerStage_[01]/.*/pwconv1/kernel', PS(None, None, 'fsdp', 'mp')),
-        # ('MetaFormerStage_[01]/.*/dwconv/kernel', PS(None, None, 'fsdp', 'mp')),
-        # ('MetaFormerStage_[01]/.*/pwconv2/kernel', PS(None, None, 'fsdp', 'mp')),
-        #
-        # ('MetaFormerStage_[01]/.*/fc1/kernel', PS(None,None,'fsdp', 'mp')),
-        # ('MetaFormerStage_[01]/.*/fc2/kernel', PS(None,None,'mp', 'fsdp')),
-        #
-        # ('MetaFormerStage_[23]/.*/qkv/kernel', PS('fsdp', 'mp')),
-        # ('MetaFormerStage_[23]/.*/proj/kernel', PS('mp', 'fsdp')),
-        #
-        #
-        # ('MetaFormerStage_[23]/.*/fc1/kernel',PS('fsdp','mp')),
-        # ('MetaFormerStage_[23]/.*/fc2/kernel', PS('mp', 'fsdp')),
+        ('scale', PS('mp')),
+        ('bias', PS('mp')),
+
+        ('fc/fc1/kernel', PS('fsdp', 'mp')),
+        ('fc/fc2/kernel', PS('mp', 'fsdp')),
+        ('downsample/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+        ('stem/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+
+        ('MetaFormerStage_[01]/.*/pwconv1/kernel', PS(None, None, 'fsdp', 'mp')),
+        ('MetaFormerStage_[01]/.*/dwconv/kernel', PS(None, None, 'fsdp', 'mp')),
+        ('MetaFormerStage_[01]/.*/pwconv2/kernel', PS(None, None, 'fsdp', 'mp')),
+
+        ('MetaFormerStage_[01]/.*/fc1/kernel', PS(None,None,'fsdp', 'mp')),
+        ('MetaFormerStage_[01]/.*/fc2/kernel', PS(None,None,'mp', 'fsdp')),
+
+        ('MetaFormerStage_[23]/.*/qkv/kernel', PS('fsdp', 'mp')),
+        ('MetaFormerStage_[23]/.*/proj/kernel', PS('mp', 'fsdp')),
+
+
+        ('MetaFormerStage_[23]/.*/fc1/kernel',PS('fsdp','mp')),
+        ('MetaFormerStage_[23]/.*/fc2/kernel', PS('mp', 'fsdp')),
         ('.*', PS(None)),
     )
 
