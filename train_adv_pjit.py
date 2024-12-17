032ef80fa68b2af507bcb216e133b9fc1c5465f0
john <1165137798@qq.com>
2024-12-17 22:10:03 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -207,7 +207,7 @@ def main(configs):
         training_step_pjit = jax.jit(training_step, static_argnums=(2,),
                                      donate_argnums=(0,),
                                      # in_shardings=(train_state_sharding, sharding,),
-                                     out_shardings=(train_state_sharding,None ))
+                                     # out_shardings=(train_state_sharding,None ))
 
         if use_orbax_save:
             checkpointer = ocp.AsyncCheckpointer(ocp.PyTreeCheckpointHandler())
