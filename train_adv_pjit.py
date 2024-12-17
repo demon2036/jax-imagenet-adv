20117c799a769732a47770cd21e9e616c9b5d461
john <1165137798@qq.com>
2024-12-17 22:04:00 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -146,7 +146,7 @@ def main(configs):
 
     print(mesh_devices.shape)
     # mesh= mesh_devices.reshape(4,1, -1)
-    mesh = einops.rearrange(mesh_devices, '(a b c)-> a b c',b=1,c=4)
+    mesh = einops.rearrange(mesh_devices, 'a ( b c)-> a b c',b=1,c=4)
     print(mesh)
 
     mesh=Mesh(mesh, ('dp', 'fsdp', 'mp'))
