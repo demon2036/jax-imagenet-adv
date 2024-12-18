d99329240e62c8a50f89f0119148ae314aab28c6
john <1165137798@qq.com>
2024-12-18 18:00:19 +0800

    commit

--- models/deit.py
+++ models/deit.py
@@ -165,7 +165,7 @@ class Attention(ViTBase, nn.Module):
         k = einops.rearrange(k, 'b n (h d)-> b h n d',h=self.heads)
         v = einops.rearrange(v, 'b n (h d)-> b h n d',h=self.heads)
         # jnp.array().swapaxes()
-        z=(q@k.sw(-2,-1))/self.head_dim**0.5
+        z=(q@k.swapaxes(-2,-1))/self.head_dim**0.5
         z=nn.softmax(z)
         z=z@v
         z=einops.rearrange(z,'b h n d -> b n (h d)')
