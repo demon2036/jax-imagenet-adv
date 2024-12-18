bb0c112817aeeb1e7214d773c8cac8529560c8ad
john <1165137798@qq.com>
2024-12-18 18:46:53 +0800

    commit

--- state/state_pjit.py
+++ state/state_pjit.py
@@ -48,7 +48,7 @@ def load_pretrain(pretrained_model='convnext_base.fb_in1k',default_params=None):
 
 
 
-def create_train_state(train_state_config, image_size: int = 256, warmup_steps=1, training_steps=10,
+def create_train_state(train_state_config, image_size: int = 224, warmup_steps=1, training_steps=10,
                        grad_accum_steps=1,mesh=None
                        ):  # -> TrainState:
 
