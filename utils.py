b7bcf5ff24618a6f9f86f835b0317a6ef320486a
john <1165137798@qq.com>
2024-12-18 08:42:43 +0800

    commit

--- utils.py
+++ utils.py
@@ -256,8 +256,6 @@ def match_partition_rules(rules, params):
         # print(name,)
         if len(leaf.shape) == 0 or np.prod(leaf.shape) == 1:
             """ Don't partition scalar values. """
-            print(name)
-            return None
             return PS()
         for rule, ps in rules:
             if re.search(rule, name) is not None:
