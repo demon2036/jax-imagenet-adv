f30a70c8800e79c8823c1628465338075a23bb22
john <1165137798@qq.com>
2024-12-18 12:20:03 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -255,6 +255,7 @@ def main(configs):
                 if step==1:
                     with jax.profiler.trace("/tmp/jax-trace", create_perfetto_link=True):
                         state, metrics = training_step_pjit(state, batch, use_pgd)
+                    break
                 else:
                     state, metrics = training_step_pjit(state, batch, use_pgd)
                 # images,labels=batch
