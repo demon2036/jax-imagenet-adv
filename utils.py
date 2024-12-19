9bd7f8028261854208fcefbcba06070842a56de2
john <1165137798@qq.com>
2024-12-19 23:04:58 +0800

    commit

--- utils.py
+++ utils.py
@@ -303,18 +303,18 @@ def get_partition_rules_caformer():
         # ('scale', PS('mp')),
         # ('bias', PS('mp')),
 
-        ('fc/fc1/kernel', PS('fsdp', 'mp')),
-        ('fc/fc2/kernel', PS('mp', None)),
-
-        ('downsample/conv/kernel', PS(None, None, 'fsdp', 'mp')),
-        ('stem/conv/kernel', PS(None, None, 'fsdp', 'mp')),
-
-        ('MetaFormerStage_[01]/.*/pwconv1/kernel', PS(None, None, 'fsdp', 'mp')),
-        ('MetaFormerStage_[01]/.*/dwconv/kernel', PS(None, None, 'fsdp', 'mp')),
-        ('MetaFormerStage_[01]/.*/pwconv2/kernel', PS(None, None, 'fsdp', 'mp')),
-
-        ('MetaFormerStage_[01]/.*/fc1/kernel', PS(None,None,'fsdp', 'mp')),
-        ('MetaFormerStage_[01]/.*/fc2/kernel', PS(None,None,'mp', 'fsdp')),
+        # ('fc/fc1/kernel', PS('fsdp', 'mp')),
+        # ('fc/fc2/kernel', PS('mp', None)),
+        #
+        # ('downsample/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+        # ('stem/conv/kernel', PS(None, None, 'fsdp', 'mp')),
+        #
+        # ('MetaFormerStage_[01]/.*/pwconv1/kernel', PS(None, None, 'fsdp', 'mp')),
+        # ('MetaFormerStage_[01]/.*/dwconv/kernel', PS(None, None, 'fsdp', 'mp')),
+        # ('MetaFormerStage_[01]/.*/pwconv2/kernel', PS(None, None, 'fsdp', 'mp')),
+        #
+        # ('MetaFormerStage_[01]/.*/fc1/kernel', PS(None,None,'fsdp', 'mp')),
+        # ('MetaFormerStage_[01]/.*/fc2/kernel', PS(None,None,'mp', 'fsdp')),
 
         ('MetaFormerStage_[23]/.*/qkv/kernel', PS('fsdp', 'mp')),
         ('MetaFormerStage_[23]/.*/proj/kernel', PS('mp', 'fsdp')),
@@ -323,8 +323,6 @@ def get_partition_rules_caformer():
         ('MetaFormerStage_[23]/.*/fc1/kernel',PS('fsdp','mp')),
         ('MetaFormerStage_[23]/.*/fc2/kernel', PS('mp', 'fsdp')),
 
-        ('MetaFormerStage_[3]/.*/fc1/kernel', PS('fsdp', 'mp')),
-        ('MetaFormerStage_[3]/.*/fc2/kernel', PS('mp', 'fsdp')),
 
 
         ('.*', PS(None)),
