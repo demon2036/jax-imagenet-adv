2e4e0e510031bd85b9b28bbbf1a56d7f2b21628e
john <1165137798@qq.com>
2024-12-19 09:07:52 +0800

    commit

--- models/deit2.py
+++ models/deit2.py
@@ -225,7 +225,7 @@ mesh_dim = '-1,1,4'
 mesh = get_jax_mesh2(mesh_dim)
 print(mesh)
 sharding = jax.sharding.NamedSharding(
-    mesh, jax.sharding.PartitionSpec("dp", 'fsdp', 'mp'))
+    mesh, jax.sharding.PartitionSpec("dp", ))
 
 class ViT(ViTBase, nn.Module):
     def setup(self):
