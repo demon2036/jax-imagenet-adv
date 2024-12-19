9b8ae4ba5f4646aeced3ef38790aa364aeaca433
john <1165137798@qq.com>
2024-12-19 15:56:45 +0800

    commit

--- models/deit2.py
+++ models/deit2.py
@@ -267,10 +267,10 @@ class ViT(ViTBase, nn.Module):
         for layer in self.layer:
             x=nn.with_logical_constraint(x,('batch','vocab','activation_embed'))
 
-            if isinstance(x,jax._src.interpreters.ad.JVPTracer):
-                # jax.debug.visualize_array_sharding(x[0])
-                print(x.shape)
-                jax.debug.inspect_array_sharding(x,callback=print)
+            # if isinstance(x,jax._src.interpreters.ad.JVPTracer):
+            #     # jax.debug.visualize_array_sharding(x[0])
+            #     print(x.shape)
+            #     jax.debug.inspect_array_sharding(x,callback=print)
 
             # x = jax.lax.with_sharding_constraint(x, sharding_m)
             x = layer(x, det)
