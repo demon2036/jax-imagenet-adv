25e1538d5360238e9ac603a8c10ae2f6bf20d3f0
john <1165137798@qq.com>
2024-12-19 19:10:04 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -128,10 +128,10 @@ def main(configs):
     filename = os.path.join(output_dir, f"{name}-{postfix}")
     print(filename)
 
-    mesh_dim = '-1,1,1'
+    # mesh_dim = '-1,1,1'
     # mesh_dim = '-1,1,16'
     # mesh_dim = '1,1,-1'
-    # mesh_dim = '1,-1,1'
+    mesh_dim = '1,-1,1'
 
     mesh = get_jax_mesh2(mesh_dim)
     print(mesh)
