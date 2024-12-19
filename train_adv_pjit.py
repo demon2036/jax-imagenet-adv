34a1d36fe11343e1159993bdd62aaa30ddcceb26
john <1165137798@qq.com>
2024-12-19 18:17:12 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -131,6 +131,8 @@ def main(configs):
     # mesh_dim = '-1,1,1'
     # mesh_dim = '-1,1,16'
     mesh_dim = '1,1,-1'
+    mesh_dim = '1,-1,1'
+
     mesh = get_jax_mesh2(mesh_dim)
     print(mesh)
     sharding = jax.sharding.NamedSharding(
