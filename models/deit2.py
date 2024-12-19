b73c2a47dab48b663b33da259f7618f7e17c3621
john <1165137798@qq.com>
2024-12-19 13:57:31 +0800

    commit

--- models/deit2.py
+++ models/deit2.py
@@ -258,10 +258,11 @@ class ViT(ViTBase, nn.Module):
         x = self.drop(self.embed(x), det)
 
         # x=self.pre_norm(x)
-        # x = jax.lax.with_sharding_constraint(x, sharding_m)
+        x = jax.lax.with_sharding_constraint(x, sharding_m)
         for layer in self.layer:
-            # x = jax.lax.with_sharding_constraint(x, sharding_m)
             x = layer(x, det)
+            x = jax.lax.with_sharding_constraint(x, sharding_m)
+
         # x = self.norm(x)
         # x=jax.lax.with_sharding_constraint(x,sharding_m)
 
