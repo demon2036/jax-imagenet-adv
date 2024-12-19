f6fac53efc2920833a2ef498b3ede2c984d1caec
john <1165137798@qq.com>
2024-12-19 14:28:33 +0800

    commit

--- models/deit2.py
+++ models/deit2.py
@@ -259,9 +259,10 @@ class ViT(ViTBase, nn.Module):
         x=jax.lax.stop_gradient(x)
 
         # x=self.pre_norm(x)
-        # x = jax.lax.with_sharding_constraint(x, sharding_m)
+        x = jax.lax.with_sharding_constraint(x, sharding_m)
         for layer in self.layer:
             x = layer(x, det)
+            x = jax.lax.with_sharding_constraint(x, sharding_m)
             # if isinstance(x,jax._src.interpreters.ad.JVPTracer):
             #     # jax.debug.visualize_array_sharding(x[0])
             #     print(x.shape)
