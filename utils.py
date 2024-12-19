e4c8556e07d4930751826a0fd41b25c0d6fd5ff4
john <1165137798@qq.com>
2024-12-19 09:32:04 +0800

    commit

--- utils.py
+++ utils.py
@@ -375,9 +375,9 @@ def get_partition_rules_vit():
         ('attn/wo/kernel', PS( 'fsdp', 'mp', )),
 
         ('ff/w1/kernel', PS('fsdp', 'mp')),
-        # ('ff/w2/kernel', PS('mp', 'fsdp')),
+        ('ff/w2/kernel', PS('mp', 'fsdp')),
 
-        ('ff/w2/kernel', PS('fsdp','mp', )),
+        # ('ff/w2/kernel', PS('fsdp','mp', )),
 
         #
         #
