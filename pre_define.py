1f904f2ee5f246c0956b882d3df0484376158f87
john <1165137798@qq.com>
2024-12-21 18:52:16 +0800

    commit

--- pre_define.py
+++ pre_define.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import jax.numpy
 import optax
 
 from utils import modified_lamb
@@ -8,7 +9,18 @@ import numpy as np
 IMAGENET_DEFAULT_MEAN = np.array([0.485, 0.456, 0.406])
 IMAGENET_DEFAULT_STD = np.array([0.229, 0.224, 0.225])
 
+
+
+def softmax_cross_entropy_with_z_loss(logits,labels,z_loss_weight=2e-4):
+    ce_loss=optax.softmax_cross_entropy(logits,labels)
+    softmax_normalizer = logits.max(-1) ** 2
+    z_loss = z_loss_weight * softmax_normalizer.mean()
+    return ce_loss+z_loss
+
+
+
 CRITERION_COLLECTION = {
+    "ce_z_loss":softmax_cross_entropy_with_z_loss,
     "ce": optax.softmax_cross_entropy,
     "bce": lambda x, y: optax.sigmoid_binary_cross_entropy(x, y > 0).mean(-1),
 }
