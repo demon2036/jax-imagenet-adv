2f5af7ed706a969424e1633dcd863539b25eb40d
john <1165137798@qq.com>
2024-12-24 23:00:05 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -261,39 +261,39 @@ def main(configs):
 
         for step in tqdm.tqdm(range(init_step, training_steps + 1), initial=init_step, total=training_steps + 1):
             # for step in tqdm.trange(init_step, training_steps + 1, dynamic_ncols=True):
-            # for _ in range(grad_accum_steps):
-            #     # batch = jax.tree_util.tree_map(lambda x: jax.make_array_from_process_local_data(sharding,np.asarray(x))  , next(train_dataloader_iter))
-            #     batch = jax.tree_util.tree_map(lambda x: jnp.array(np.asarray(x)), next(train_dataloader_iter))
-            #     batch = jtu.tree_map_with_path(partial(_form_global_array, global_mesh=mesh), batch)
-            #     # batch = jtu.tree_map(go_jit, batch)
-            #     # images, labels = batch
-            #     # print(f'{images.shape=}   {images.addressable_data(0).shape=}')
-            #     # jax.debug.visualize_array_sharding(labels,max_width=200)
-            #     # di=flax.traverse_util.flatten_dict(state.params,sep='.')
-            #     # print(di.keys())
-            #     # jax.debug.visualize_array_sharding(di['model.MetaFormerStage_2.MetaFormerBlock_7.mlp.fc1.kernel'])
-            #     # print(jax.devices())
-            #     # while True:
-            #     #     pass
-            #
-            #     state, metrics = training_step_pjit(state, batch, use_pgd)
-            # #     # state, metrics = training_step(state, batch, use_pgd)
-            #     average_meter.update(**metrics)
-            #
-            #
-            # if step % epoch_per_step == 0:
-            #     epoch = step // epoch_per_step
-            #     mix_ratio_state.update_mix_ratio(epoch, configs['training_epoch'])
-            #
-            # if (
-            #         jax.process_index() == 0
-            #         and log_interval > 0
-            #         and step % log_interval == 0
-            # ):
-            #     metrics = average_meter.summary(prefix="train/")
-            #     metrics["processed_samples"] = step * configs['dataset']['train_batch_size']
-            #     metrics["mix_ratio"] = mix_ratio_state.ratio
-            #     wandb.log(metrics, step)
+            for _ in range(grad_accum_steps):
+                # batch = jax.tree_util.tree_map(lambda x: jax.make_array_from_process_local_data(sharding,np.asarray(x))  , next(train_dataloader_iter))
+                batch = jax.tree_util.tree_map(lambda x: jnp.array(np.asarray(x)), next(train_dataloader_iter))
+                batch = jtu.tree_map_with_path(partial(_form_global_array, global_mesh=mesh), batch)
+                # batch = jtu.tree_map(go_jit, batch)
+                # images, labels = batch
+                # print(f'{images.shape=}   {images.addressable_data(0).shape=}')
+                # jax.debug.visualize_array_sharding(labels,max_width=200)
+                # di=flax.traverse_util.flatten_dict(state.params,sep='.')
+                # print(di.keys())
+                # jax.debug.visualize_array_sharding(di['model.MetaFormerStage_2.MetaFormerBlock_7.mlp.fc1.kernel'])
+                # print(jax.devices())
+                # while True:
+                #     pass
+
+                state, metrics = training_step_pjit(state, batch, use_pgd)
+            #     # state, metrics = training_step(state, batch, use_pgd)
+                average_meter.update(**metrics)
+
+
+            if step % epoch_per_step == 0:
+                epoch = step // epoch_per_step
+                mix_ratio_state.update_mix_ratio(epoch, configs['training_epoch'])
+
+            if (
+                    jax.process_index() == 0
+                    and log_interval > 0
+                    and step % log_interval == 0
+            ):
+                metrics = average_meter.summary(prefix="train/")
+                metrics["processed_samples"] = step * configs['dataset']['train_batch_size']
+                metrics["mix_ratio"] = mix_ratio_state.ratio
+                wandb.log(metrics, step)
 
             if eval_interval > 0 and (
                     step % eval_interval == 0 or step == training_steps
