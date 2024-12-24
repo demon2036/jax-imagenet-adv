2d9cb69508b26f1158ca982f43ff36aa9397c3b7
john <1165137798@qq.com>
2024-12-24 23:51:04 +0800

    commit

--- state/state_pjit.py
+++ state/state_pjit.py
@@ -9,6 +9,7 @@ import optax
 import timm
 from jax._src.pjit import pjit
 
+import train_modules
 from pre_define import CRITERION_COLLECTION, OPTIMIZER_COLLECTION
 from training import TrainState
 from utils import read_yaml, get_obj_from_str, Mixup, preprocess_config, match_partition_rules, \
@@ -220,6 +221,144 @@ def create_train_state(train_state_config, image_size: int = 224, warmup_steps=1
         # print(state)
 
     return state,train_state_partition,train_state_sharding
+
+
+
+
+
+
+
+
+
+
+
+def create_train_state_restore(train_state_config, image_size: int = 224, warmup_steps=1, training_steps=10,
+                       grad_accum_steps=1,mesh=None,logical_axis_rules=None
+                       ):  # -> TrainState:
+
+
+    model_config = train_state_config['model']
+    optimizer_config = train_state_config['optimizer']
+    train_module_config = train_state_config['train_module']
+    pretrained_ckpt=train_state_config.pop('pretrained_ckpt',None)
+
+    model = get_obj_from_str(model_config['target'])(**model_config['model_kwargs'])
+    print(f'{train_module_config=}')
+
+    train_module=train_modules.TrainModule
+
+
+    module = train_module(
+        model=model,
+        mixup=Mixup(train_module_config.pop('mixup',), train_module_config.pop('cutmix')),
+        label_smoothing=train_module_config.pop('label_smoothing') if train_module_config['criterion'] != "bce" else 0,
+        criterion=CRITERION_COLLECTION[train_module_config.pop('criterion')],
+    )
+
+
+    # Initialize the model weights with dummy inputs. Using the init RNGS and inputs, we
+    # will tabulate the summary of model and its parameters. Furthermore, empty gradient
+    # accumulation arrays will be prepared if the gradient accumulation is enabled.
+    example_inputs = {
+        "images": jnp.zeros((1, 3, image_size, image_size), dtype=jnp.uint8),
+        # "labels": jnp.zeros((1,), dtype=jnp.int32),
+        "labels": jnp.ones((1,),dtype=jnp.int32)#jnp.array([1,2], dtype=jnp.int32),
+    }
+
+    init_rngs = {"params": jax.random.PRNGKey(train_state_config['init_seed'])}
+
+    if jax.process_index()==0:
+        print(module.tabulate(init_rngs, **example_inputs,
+                              depth=2,compute_flops=True,console_kwargs={'width': 160},
+                              compute_vjp_flops=True))
+
+    params = module.init(init_rngs, **example_inputs,det=False)["params"]
+
+    lr = optimizer_config['optimizer_kwargs'].pop('learning_rate')
+    end_lr = optimizer_config['optimizer_kwargs'].pop('end_learning_rate',1e-5)
+    schedule = optimizer_config['optimizer_kwargs'].pop('schedule','cosine')
+
+    # Create learning rate scheduler and optimizer with gradient clipping. The learning
+    # rate will be recorded at `hyperparams` by `optax.inject_hyperparameters`.
+    @partial(optax.inject_hyperparams, hyperparam_dtype=jnp.float32)
+    def create_optimizer_fn(
+            learning_rate: optax.Schedule,
+    ) -> optax.GradientTransformation:
+        tx = OPTIMIZER_COLLECTION[optimizer_config['target']](
+            learning_rate=learning_rate,
+            **optimizer_config['optimizer_kwargs'],
+            mask=partial(jax.tree_util.tree_map_with_path, lambda kp, *_: kp[-1].key == "kernel"),
+        )
+        tx = optax.chain(optax.clip_by_global_norm(1.0), tx)
+        return tx
+
+
+    if schedule !='cosine':
+        learning_rate=lr
+    else:
+        learning_rate = optax.warmup_cosine_decay_schedule(
+            init_value=1e-6,
+            peak_value=lr,
+            warmup_steps=warmup_steps,
+            decay_steps=training_steps,
+            end_value=end_lr,
+        )
+
+
+    def init_fn(params)->TrainState:
+        tx = create_optimizer_fn(learning_rate)
+
+        if grad_accum_steps > 1:
+            grad_accum = jax.tree_map(jnp.zeros_like, params)
+
+        state= TrainState.create(
+            apply_fn=module.apply,
+            params=params,
+            tx=tx,
+            mixup_rng=jax.random.PRNGKey(train_state_config['mixup_seed']),
+            dropout_rng=jax.random.PRNGKey(train_state_config['dropout_seed'] ),
+            adv_rng=jax.random.PRNGKey(2036 ),
+            ema_decay=train_state_config['ema_decay'],
+            ema_params=copy.deepcopy(params) if train_state_config['ema_decay'] > 0 else None,
+            micro_step=0,
+            micro_in_mini=grad_accum_steps,
+            grad_accum=grad_accum if grad_accum_steps > 1 else None,
+        )
+        return state
+
+    train_state_shapes = jax.eval_shape(init_fn, params)
+    train_state_partition = match_partition_rules(get_partition_rules_caformer(), train_state_shapes)
+    train_state_sharding = jax.tree_util.tree_map(lambda x: jax.sharding.NamedSharding(mesh, x), train_state_partition)
+
+
+
+    state=jax.jit(init_fn, #in_shardings=(train_state_partition.params, ),
+        out_shardings=train_state_sharding,
+                  )(params)
+
+
+    if jax.process_index()==0:
+        print(train_state_config)
+        # print(state)
+
+    return state,train_state_partition,train_state_sharding
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -300,34 +300,32 @@ def main(configs):
             ):
                 if valid_dataloader is None:
                     continue
-                try:
-                    metrics = evaluate(state, valid_dataloader,validation_adv_step_jited,mesh)
-                    print(metrics)
+                metrics = evaluate(state, valid_dataloader,validation_adv_step_jited,mesh)
+                print(metrics)
 
-                    if ["val/advacc1"] in metrics:
-                        now_acc1=metrics["val/advacc1"]
+                if ["val/advacc1"] in metrics:
+                    now_acc1=metrics["val/advacc1"]
+                else:
+                    now_acc1=metrics["val/acc1"]
+
+                if now_acc1 > max_val_acc1:
+                    if use_orbax_save:
+                        ckpt = {'model': state}
+                        save_args = orbax_utils.save_args_from_target(ckpt)
+                        checkpointer.save(filename, ckpt, save_args=save_args, force=True)
                     else:
-                        now_acc1=metrics["val/acc1"]
+                        if jax.process_index() == 0:
+                            params_bytes = msgpack_serialize(unreplicate(state.ema_params))
+                            save_checkpoint_in_background(filename, params_bytes, postfix="last")
 
-                    if now_acc1 > max_val_acc1:
-                        if use_orbax_save:
-                            ckpt = {'model': state}
-                            save_args = orbax_utils.save_args_from_target(ckpt)
-                            checkpointer.save(filename, ckpt, save_args=save_args, force=True)
-                        else:
-                            if jax.process_index() == 0:
-                                params_bytes = msgpack_serialize(unreplicate(state.ema_params))
-                                save_checkpoint_in_background(filename, params_bytes, postfix="last")
+                    max_val_acc1 = metrics["val/advacc1"]
+                    # save_checkpoint_in_background(args, params_bytes, postfix="best")
 
-                        max_val_acc1 = metrics["val/advacc1"]
-                        # save_checkpoint_in_background(args, params_bytes, postfix="best")
+                metrics["val/acc1/best"] = max_val_acc1
+                metrics["processed_samples"] = step * configs['dataset']['train_batch_size']
+                if jax.process_index() == 0:
+                    wandb.log(metrics, step)
 
-                    metrics["val/acc1/best"] = max_val_acc1
-                    metrics["processed_samples"] = step * configs['dataset']['train_batch_size']
-                    if jax.process_index() == 0:
-                        wandb.log(metrics, step)
-                except Exception as e:
-                    print(e)
             if use_orbax_save:
                 checkpointer.wait_until_finished()
 
