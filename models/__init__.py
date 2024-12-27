13f0a7aa3f4f3f0a4c86cd8821b82784c1c0056c
john <1165137798@qq.com>
2024-12-27 10:52:27 +0800

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .convnext import ConvNeXt
 from .deit2 import ViT
-from .metaformer2 import MetaFormer,CAFormer,ConvFormer
+from .metaformer2 import MetaFormer,CAFormer,ConvFormer,CAFormerQKNorm
