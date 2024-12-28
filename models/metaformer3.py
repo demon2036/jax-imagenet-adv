c1d2825a67c711c30b5d4afa86841006600026b9
john <1165137798@qq.com>
2024-12-28 19:13:12 +0800

    commit

--- models/metaformer3.py
+++ models/metaformer3.py
@@ -52,8 +52,7 @@ class Mlp(nn.Module):
         if self.norm_layer:
             x = self.norm_layer()(x)
 
-        if not self.use_conv:
-            x=nn.LayerNorm(dtype=dtype,reduction_axes=-2,feature_axes=-2,use_bias=False,use_scale=False)(x)
+
 
 
         # Second layer
@@ -249,6 +248,9 @@ class Attention(nn.Module):
             x = jnp.einsum("...nm,...md->...nd", attn, v)
 
         x = x.transpose((0, 2, 1, 3)).reshape(B, N, C)
+
+        x = nn.LayerNorm(dtype=dtype, reduction_axes=-2, feature_axes=-2, use_bias=False, use_scale=False)(x)
+
         x = nn.Dense(C, use_bias=self.proj_bias, name="proj",dtype = dtype)(x)
         x = nn.Dropout(self.proj_drop)(x, deterministic=det)
         return x
