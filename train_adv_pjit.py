7f2f27a81f15e453c524dd179101be38a96a6381
john <1165137798@qq.com>
2024-12-29 22:11:22 +0800

    commit

--- train_adv_pjit.py
+++ train_adv_pjit.py
@@ -467,7 +467,7 @@ if __name__ == "__main__":
     yaml = read_yaml('configs/planB/ablation/best/caformer-xxl-48-192-3step-700ep-real-adv-step-3-rand-mix0.9-lamb.yaml')
     # yaml = read_yaml('configs/planB/ablation/standard/caformer-b-36-silu-standard-300ep-mix0.9-modified_lion.yaml')
     yaml = preprocess_config(yaml)
-    # jax.distributed.initialize()
+    jax.distributed.initialize()
 
 
     # print(yaml)
