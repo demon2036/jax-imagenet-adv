efea1acf1ddf6371456996c87e7aaadf04fbd6e4
john <1165137798@qq.com>
2024-12-31 15:29:46 +0800

    commit

--- state/state_pjit2.py
+++ state/state_pjit2.py
@@ -258,7 +258,7 @@ def init_state(train_state_config, image_size: int = 224, warmup_steps=1, traini
 
     def set_sharding(x: jax.ShapeDtypeStruct,sharding) -> StandardRestoreArgs:
         x.sharding = sharding
-        return ocp.args.ArrayRestore(x)
+        return x
         # return x
 
     restore_args={'model':jax.tree_util.tree_map(set_sharding,state_shapes,train_state_sharding)}
@@ -279,7 +279,7 @@ def init_state(train_state_config, image_size: int = 224, warmup_steps=1, traini
     try:
         state = checkpointer.restore(pretrained_ckpt, #item=ckpt,#restore_args=restore_args
                                     # **restore_kwargs
-                                     args=ocp.args.PyTreeRestore(change_sharding_abstract_state),
+
 
 
                                      )['model']
