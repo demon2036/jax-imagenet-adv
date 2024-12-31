2cb78b076333c62ef2e262587ec8c76c9ee1c61c
john <1165137798@qq.com>
2024-12-31 16:44:39 +0800

    commit

--- train_pjit.py
+++ train_pjit.py
@@ -208,6 +208,7 @@ def main(configs):
                 # batch = jax.tree_util.tree_map(lambda x: jax.make_array_from_process_local_data(sharding,np.asarray(x))  , next(train_dataloader_iter))
                 batch = jax.tree_util.tree_map(lambda x: jnp.array(np.asarray(x)), next(train_dataloader_iter))
                 batch = jtu.tree_map_with_path(partial(_form_global_array, global_mesh=mesh), batch)
+                print(batch[0].shape)
                 # batch = jtu.tree_map(go_jit, batch)
                 # images, labels = batch
                 # print(f'{images.shape=}   {images.addressable_data(0).shape=}')
