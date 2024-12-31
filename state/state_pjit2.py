740eb0dcd6062d4c9f1a5c2f8ae92e87d267d65d
john <1165137798@qq.com>
2024-12-31 16:55:08 +0800

    commit

--- state/state_pjit2.py
+++ state/state_pjit2.py
@@ -270,7 +270,8 @@ def init_state(train_state_config, image_size: int = 224, warmup_steps=1, traini
     if pretrained_ckpt is not None:
         state = resume_checkpoint(pretrained_ckpt, restore_state_shapes, restore_state_sharding)
         params=state['model'].ema_params
-        state=jax.jit(init_by_params_fn,out_shardings=train_state_sharding)(params)
+        del state
+        state=jax.jit(init_by_params_fn,out_shardings=train_state_sharding,donate_argnums=(0,))(params)
     else:
         state=jax.jit(init_fn,out_shardings=train_state_sharding)(init_rngs,example_inputs)
 
