c8ee82cc033d7287fb69a9a15198ed9c9eec9f33
john <1165137798@qq.com>
2025-01-01 11:52:41 +0800

    commit

--- state/state_pjit2.py
+++ state/state_pjit2.py
@@ -9,6 +9,7 @@ import numpy as np
 import optax
 import timm
 from jax._src.pjit import pjit
+from optax.schedules import _join
 from orbax.checkpoint._src.handlers.standard_checkpoint_handler import StandardRestoreArgs
 from orbax.checkpoint._src.serialization.type_handlers import ArrayRestoreArgs
 
@@ -56,7 +57,50 @@ def load_pretrain(pretrained_model='convnext_base.fb_in1k', default_params=None)
 
 
 
+def warmup_stable_cosine_decay_schedule(
+        init_value: float,
+        peak_value: float,
+        warmup_steps: int,
+        decay_steps: int,
+        end_value: float = 0.0,
+        exponent: float = 1.0,
+) -> base.Schedule:
+    r"""Linear warmup followed by cosine decay.
 
+    Args:
+      init_value: Initial value for the scalar to be annealed.
+      peak_value: Peak value for scalar to be annealed at end of warmup.
+      warmup_steps: Positive integer, the length of the linear warmup.
+      decay_steps: Positive integer, the total length of the schedule. Note that
+        this includes the warmup time, so the number of steps during which cosine
+        annealing is applied is ``decay_steps - warmup_steps``.
+      end_value: End value of the scalar to be annealed.
+      exponent: The default decay is ``0.5 * (1 + cos(pi t/T))``, where ``t`` is
+        the current timestep and ``T`` is ``decay_steps``. The exponent modifies
+        this to be ``(0.5 * (1 + cos(pi * t/T))) ** exponent``. Defaults to 1.0.
+
+    Returns:
+      schedule
+        A function that maps step counts to values
+    """
+    alpha = 0.0 if peak_value == 0.0 else end_value / peak_value
+    schedules = [
+        optax.schedules.linear_schedule(
+            init_value=init_value,
+            end_value=peak_value,
+            transition_steps=warmup_steps,
+        ),
+        optax.schedules.constant_schedule(
+           value=peak_value
+        ),
+        optax.schedules.cosine_decay_schedule(
+            init_value=peak_value,
+            decay_steps=decay_steps - warmup_steps,
+            alpha=alpha,
+            exponent=exponent,
+        ),
+    ]
+    return _join.join_schedules(schedules, [warmup_steps,int(decay_steps*2/3)])
 
 
 def create_train_state(train_state_config, image_size: int = 224,
@@ -105,8 +149,6 @@ def create_train_state(train_state_config, image_size: int = 224,
     # Create learning rate scheduler and optimizer with gradient clipping. The learning
     # rate will be recorded at `hyperparams` by `optax.inject_hyperparameters`.
 
-
-
     @partial(optax.inject_hyperparams, hyperparam_dtype=jnp.float32)
     def create_optimizer_fn(
             learning_rate: optax.Schedule,
@@ -124,7 +166,7 @@ def create_train_state(train_state_config, image_size: int = 224,
         return tx
 
 
-    if schedule != 'cosine':
+    if schedule == 'constant':
         learning_rate = optax.warmup_cosine_decay_schedule(
             init_value=lr,
             peak_value=lr,
@@ -132,6 +174,17 @@ def create_train_state(train_state_config, image_size: int = 224,
             decay_steps=training_steps,
             end_value=lr,
         )
+
+    elif schedule=="wsd":
+        learning_rate = warmup_stable_cosine_decay_schedule(
+            init_value=init_value,
+            peak_value=lr,
+            warmup_steps=warmup_steps,
+            decay_steps=training_steps,
+            end_value=end_lr,
+        )
+
+
     else:
         learning_rate = optax.warmup_cosine_decay_schedule(
             init_value=init_value,
