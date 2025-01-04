9f741c5628ba9aeb25da91c69c4f241d65d92e56
john <1165137798@qq.com>
2025-01-05 01:23:19 +0800

    commit

--- optimizer/modified_adamw.py
+++ optimizer/modified_adamw.py
@@ -259,9 +259,9 @@ def modified_lamb2(
 ) -> optax.GradientTransformation:
     return optax.chain(
         scale_by_adam(b1=b1, b2=b2, eps=eps, eps_root=eps_root),
-        add_decayed_weights(weight_decay=weight_decay, mask=mask),
+        # add_decayed_weights(weight_decay=weight_decay, mask=mask),
         # Change to use trust ratio on weight decay parameters only.
-        optax.masked(optax.scale_by_trust_ratio(), mask=mask),
+        # optax.masked(optax.scale_by_trust_ratio(), mask=mask),
         scale_by_scale(),
         optax.scale_by_learning_rate(learning_rate),
     )
