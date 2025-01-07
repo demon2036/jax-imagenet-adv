29bf04d3d00763246f5cd07848b2c778e1a99bf2
john <1165137798@qq.com>
2025-01-07 21:58:40 +0800

    commit

--- train_modules/train_module.py
+++ train_modules/train_module.py
@@ -9,6 +9,7 @@ import optax
 from chex import Array, ArrayTree
 
 from attacks import pgd_attack, trade
+from attacks.pgd import pgd_dynamic_scale_attack
 from attacks.trades import trade_lse
 from utils import Mixup
 
@@ -201,6 +202,83 @@ class TrainAdvModule(nn.Module):
                                         step_size=self.train_adv_step_size,  #if train else self.test_adv_step_size ,
                                         maxiter=self.train_adv_step  #if train else self.test_adv_step
                                         )
+
+
+        loss = self.criterion((logits := self.model(images, det=det)), labels)
+        labels = labels == labels.max(-1, keepdims=True)
+
+        # Instead of directly comparing the maximum classes of predicted logits with the
+        # given one-hot labels, we will check if the predicted classes are within the
+        # label set. This approach is equivalent to traditional methods in single-label
+        # classification and also supports multi-label tasks.
+        preds = jax.lax.top_k(logits, k=5)[1]
+        accs = jnp.take_along_axis(labels, preds, axis=-1)
+        if return_logits:
+            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1),'logits':logits}
+        else:
+            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TrainAdvModule2(nn.Module):
+
+    model: Any
+    mixup: Mixup
+    label_smoothing: float = 0.0
+    criterion: Callable[[Array, Array], Array] = CRITERION_COLLECTION["ce"]
+
+    # train_adv_step:int=3
+    # train_adv_step_size: float = 4 / 3 / 255
+
+    train_adv_step: int = 10
+    train_adv_step_size: float = 1 / 255
+
+    # test_adv_step: int = 10
+    #
+    # test_adv_step: int = 10
+
+    # test_adv_step_size: float = 1 / 255
+
+    eps: float = 4 / 255
+    use_pgd: bool = False
+    beta: float = 0.0
+
+    def __call__(self, images: Array, labels: Array, det: bool = True, use_pgd=True, use_trade=False,
+                 ref=False,return_logits=False) -> ArrayTree:
+        # Normalize the pixel values in TPU devices, instead of copying the normalized
+        # float values from CPU. This may reduce both memory usage and latency.
+        images = jnp.moveaxis(images, 1, 3).astype(jnp.float32) / 0xFF
+
+        labels = nn.one_hot(labels, self.model.labels) if labels.ndim == 1 else labels
+        labels = labels.astype(jnp.float32)
+
+        if not det:
+            labels = optax.smooth_labels(labels, self.label_smoothing)
+            images, labels = self.mixup(images, labels)
+        print(use_pgd,use_trade,)
+
+
+
+        if ref:
+            pass
+        else:
+            images = pgd_dynamic_scale_attack(images, labels, self.model, key=self.make_rng('adv'),epsilon=self.eps,
+                                step_size=self.train_adv_step_size,  #if train else self.test_adv_step_size ,
+                                maxiter=self.train_adv_step,  #if train else self.test_adv_step
+                                              dynamic=not det
+                                )
 
 
         loss = self.criterion((logits := self.model(images, det=det)), labels)
