bc77b92384dd03b753a8c2154a30c6e637f44b7d
john <1165137798@qq.com>
2025-01-17 21:04:38 +0800

    commit

--- train_pjit.py
+++ train_pjit.py
@@ -88,7 +88,7 @@ def _form_global_array(path, array: np.ndarray, global_mesh: Mesh) -> jax.Array:
 
 def evaluate(state: TrainState, dataloader: DataLoader,validation_adv_step_jited,mesh) -> dict[str, float]:
     average_meter = AverageMeter()
-    print(len(dataloader))
+    # print(len(dataloader))
     counter=0
     for batch in tqdm.tqdm(dataloader, leave=False, dynamic_ncols=True):
         batch = jax.tree_util.tree_map(lambda x: jnp.array(np.asarray(x)), batch)
@@ -98,8 +98,8 @@ def evaluate(state: TrainState, dataloader: DataLoader,validation_adv_step_jited
         print(counter)
         counter+=1
 
-        metrics = validation_adv_step_jited(state, batch)
-        average_meter.update(**metrics)
+        # metrics = validation_adv_step_jited(state, batch)
+        # average_meter.update(**metrics)
 
     metrics = average_meter.summary("val/")
     num_samples = metrics.pop("val/num_samples")
