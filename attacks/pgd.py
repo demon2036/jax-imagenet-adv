0062e644334089fcc511447152e79ad07817eba0
john <1165137798@qq.com>
2025-02-11 15:53:44 +0800

    commit

--- attacks/pgd.py
+++ attacks/pgd.py
@@ -117,12 +117,13 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
 
 
     # if dynamic:
-    #     image_perturbation_zero=jnp.zeros_like(image)
-    #     image_perturbation = jnp.concatenate([image_perturbation[None,...], image_perturbation_zero[None,...]], axis=0)
-    #     image_perturbation=jax.random.choice(key2, image_perturbation, p=jnp.array([0.5, 0.5]))
+    #     # image_perturbation_zero=jnp.zeros_like(image)
+    #     # image_perturbation = jnp.concatenate([image_perturbation[None,...], image_perturbation_zero[None,...]], axis=0)
+    #     # image_perturbation=jax.random.choice(key2, image_perturbation, p=jnp.array([0.5, 0.5]))
     #
     #
-    #     step_size=jax.random.uniform(key2,(1,),minval=0.5,maxval=1).reshape((-1,1,1,1))*step_size
+    #     # step_size=jax.random.uniform(key2,(1,),minval=0.5,maxval=1).reshape((-1,1,1,1))*step_size
+    #     adv_step_size=jax.random.uniform(key2,(1,),minval=0.5,maxval=1).reshape((-1,1,1,1))*step_size
 
     # print(label)
 
@@ -145,8 +146,8 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
         if dynamic:
             # key1, key2 = jax.random.split(key2)
             # adv_step_size = jax.random.uniform(key2,(1,),minval=0.5,maxval=1.5).reshape((-1,1,1,1))*step_size
-            adv_step_size = jax.random.uniform(key2, (1,), minval=0.7, maxval=1.2).reshape((-1, 1, 1, 1)) * step_size
-            # adv_step_size = jax.random.uniform(key3, (image.shape[0],), minval=0.5, maxval=1.5).reshape((-1, 1, 1, 1)) * step_size
+            # adv_step_size = jax.random.uniform(key2, (1,), minval=0.7, maxval=1.2).reshape((-1, 1, 1, 1)) * step_size
+            adv_step_size = jax.random.uniform(key3, (image.shape[0],), minval=0.5, maxval=1.5).reshape((-1, 1, 1, 1)) * step_size
         else:
             adv_step_size = step_size
         # if dynamic:
