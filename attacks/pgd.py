09676671015a54fdf49b34f4051fc9fdb07d5a53
john <1165137798@qq.com>
2025-02-14 14:57:42 +0800

    commit

--- attacks/pgd.py
+++ attacks/pgd.py
@@ -111,7 +111,7 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
 
     # image_perturbation = jnp.zeros_like(image)
 
-    key1,key2,key3,key4=jax.random.split(key,4)
+    key1,key2,key3,key4,key5=jax.random.split(key,5)
 
     image_perturbation = jax.random.uniform(key1, image.shape, minval=-epsilon, maxval=epsilon)
 
@@ -123,8 +123,18 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
     #
     #
     #     # step_size=jax.random.uniform(key2,(1,),minval=0.5,maxval=1).reshape((-1,1,1,1))*step_size
-        adv_step_size=jax.random.uniform(key2,(1,),minval=0.5,maxval=1).reshape((-1,1,1,1))*step_size
-        epsilon = jax.random.uniform(key4, (1,), minval = 1.0, maxval = 1.5).reshape((-1,1,1,1)) *epsilon
+        adv_step_size=jax.random.uniform(key2,(1,),minval=0.75,maxval=1).reshape((-1,1,1,1))*step_size
+        # epsilon = jax.random.uniform(key4, (1,), minval = 1.0, maxval = 1.5).reshape((-1,1,1,1)) *epsilon
+        epsilon_extend = jax.random.uniform(key4, (image.shape[0],), minval=1.5, maxval=2.0) * epsilon
+
+        r = jax.randomrandom.uniform(key, shape=(image.shape[0],))
+        sorted_indices = jnp.argsort(r)
+        mask = jnp.zeros_like(sorted_indices, dtype=bool)
+        mask = mask.at[sorted_indices[-10:]].set(True)
+        epsilon = jnp.where(mask, epsilon_extend, epsilon)
+
+
+    #.reshape((-1, 1, 1, 1)
 
     # epsilon=jax.random.uniform(key4,(image.shape[0],),minval=0.95,maxval=1.05).reshape((-1, 1, 1, 1))*epsilon
         # epsilon=epsilon*2
