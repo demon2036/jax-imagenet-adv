c7387fe5feecb47e519631e58a7afda515f07322
john <1165137798@qq.com>
2025-02-16 14:32:37 +0800

    commit

--- attacks/pgd.py
+++ attacks/pgd.py
@@ -277,6 +277,9 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
         grad=grad_adversarial(image_perturbation)
 
         metrics[f'top_p_{i}'],factor=_nucleus_sampling(logits=einops.rearrange(grad,'b h w c -> b (h w c)'))
+
+
+        print(grad.shape)
         metrics[f'norm_{i}']=jnp.linalg.norm(grad,ord=2,axis=(1,2,3)).mean()
         sign_grad = jnp.sign(grad)
 
