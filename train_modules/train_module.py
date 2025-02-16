cc9369aeb4f13bf314030fab454586673a76fb39
john <1165137798@qq.com>
2025-02-16 18:05:39 +0800

    commit

--- train_modules/train_module.py
+++ train_modules/train_module.py
@@ -135,7 +135,7 @@ class TrainAdvModule(nn.Module):
             images, labels = self.mixup(images, labels)
         print(use_pgd,use_trade,)
 
-
+        metrics={}
 
         if ref:
             pass
@@ -197,7 +197,7 @@ class TrainAdvModule(nn.Module):
 
 
                     # images=jax.lax.cond(jax.random.uniform(self.make_rng('adv'),(1,))[0]<0.9,test1,test2   )
-                    images = pgd_attack(images, labels, self.model, key=self.make_rng('adv'),epsilon=self.eps,
+                    images, metrics = pgd_attack(images, labels, self.model, key=self.make_rng('adv'),epsilon=self.eps,
                                         step_size=self.train_adv_step_size,  #if train else self.test_adv_step_size ,
                                         maxiter=self.train_adv_step  #if train else self.test_adv_step
                                         )
@@ -213,9 +213,9 @@ class TrainAdvModule(nn.Module):
         preds = jax.lax.top_k(logits, k=5)[1]
         accs = jnp.take_along_axis(labels, preds, axis=-1)
         if return_logits:
-            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1),'logits':logits}
+            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1),'logits':logits} |metrics
         else:
-            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1)}
+            return {"loss": loss, "acc1": accs[:, 0], "acc5": accs.any(-1)} |metrics
 
 
 
