09988e4eee0d7c0085c7fdc4b1b3f150b37e21fd
john <1165137798@qq.com>
2025-02-16 15:15:51 +0800

    commit

--- attacks/pgd.py
+++ attacks/pgd.py
@@ -282,7 +282,8 @@ def pgd_dynamic_scale_attack(image, label, model, epsilon=4 / 255, step_size=4/3
         sign_grad = jnp.sign(grad)
 
         if dynamic:
-            sign_grad*=einops.rearrange(factor,'b (h w c)-> b h w c',h=h,w=w,c=c)
+            pass
+            # sign_grad*=einops.rearrange(factor,'b (h w c)-> b h w c',h=h,w=w,c=c)
         # heuristic step-size 2 eps / maxiter
         image_perturbation += adv_step_size * sign_grad
         # projection step onto the L-infinity ball centered at image
