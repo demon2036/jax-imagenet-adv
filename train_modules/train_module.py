dba7763e17630a7174c75ffd552030862803e0f1
john <1165137798@qq.com>
2025-03-05 12:11:59 +0800

    commit

--- train_modules/train_module.py
+++ train_modules/train_module.py
@@ -279,19 +279,19 @@ class TrainAdvModule2(nn.Module):
             pass
         else:
             if use_pgd:
-                images,metrics = pgd_dynamic_scale_attack(images, labels, self.model, key=self.make_rng('adv'),epsilon=self.eps,
-                                    step_size=self.train_adv_step_size,  #if train else self.test_adv_step_size ,
-                                    maxiter=self.train_adv_step,  #if train else self.test_adv_step
-                                                  dynamic=not det
-                                    )
-                # images, metrics = rgd_dynamic_scale_attack(images, labels, self.model, key=self.make_rng('adv'),
-                #                                            epsilon=self.eps,
-                #                                            step_size=self.train_adv_step_size,
-                #                                            # if train else self.test_adv_step_size ,
-                #                                            maxiter=self.train_adv_step,
-                #                                            # if train else self.test_adv_step
-                #                                            dynamic=not det
-                #                                            )
+                # images,metrics = pgd_dynamic_scale_attack(images, labels, self.model, key=self.make_rng('adv'),epsilon=self.eps,
+                #                     step_size=self.train_adv_step_size,  #if train else self.test_adv_step_size ,
+                #                     maxiter=self.train_adv_step,  #if train else self.test_adv_step
+                #                                   dynamic=not det
+                #                     )
+                images, metrics = rgd_dynamic_scale_attack(images, labels, self.model, key=self.make_rng('adv'),
+                                                           epsilon=self.eps,
+                                                           step_size=self.train_adv_step_size,
+                                                           # if train else self.test_adv_step_size ,
+                                                           maxiter=self.train_adv_step,
+                                                           # if train else self.test_adv_step
+                                                           dynamic=not det
+                                                           )
 
         loss = self.criterion((logits := self.model(images, det=det)), labels)
         labels = labels == labels.max(-1, keepdims=True)
