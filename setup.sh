dc3525bb7b633a0e7fe364b97a65a637455504ef
john <1165137798@qq.com>
2025-05-10 16:19:06 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -30,9 +30,9 @@ pip install einops  tensorflow matplotlib diffusers
 pip install gcsfs tpu-info
 
 
-pip install orbax-checkpoint== 0.11.0
+#pip install orbax-checkpoint== 0.11.0
 
-pip install tensorstore==0.1.68
+#pip install tensorstore==0.1.68
 
 
 
