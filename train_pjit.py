a122d42338922c532571c82d1b8c81305654dfca
john <1165137798@qq.com>
2025-05-10 14:33:07 +0800

    commit

--- train_pjit.py
+++ train_pjit.py
@@ -427,9 +427,7 @@ if __name__ == "__main__":
     # yaml = read_yaml('configs/planB/ablation/best/test.yaml')
     yaml = preprocess_config(yaml)
 
-    try:
 
-        jax.distributed.initialize()
-        main(yaml)
-    except Exception as e:
-        print(e)
+    jax.distributed.initialize()
+    main(yaml)
+
